866ff316bae29427bdd2c89327bf95b490e5b286
ebnach <111057768+ebnach@users.noreply.github.com>
2024-10-05 03:58:02 +0000

    little api

--- module-1/src/main.rs
+++ module-1/src/main.rs
@@ -1,5 +1,21 @@
 //! This is a documentation comment for the `module_1` crate.
+//! 
 
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world22!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // lets create web server
+    
+    HttpServer::new(|| {
+        App::new().service(index)
+    })
+    .bind("localhost:8080")?
+    .run()
+    .await
 }
